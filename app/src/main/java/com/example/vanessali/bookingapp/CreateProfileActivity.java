c3693eaaee2b608a8481c1512be660bdca741e1e
vanessali <li.vanessaa@gmail.com>
2019-05-01 18:29:19 -0400

    added new activity

---
+++ app/src/main/java/com/example/vanessali/bookingapp/CreateProfileActivity.java
@@ -0,0 +1,13 @@
+package com.example.vanessali.bookingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_profile);
+    }
+}

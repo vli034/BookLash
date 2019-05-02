0a96419913503f5adb524ff72ec1e5e15312da30
vanessali <li.vanessaa@gmail.com>
2019-05-01 20:56:45 -0400

    toolbar xml

--- app/src/main/java/com/example/vanessali/bookingapp/MainActivity.java
+++ app/src/main/java/com/example/vanessali/bookingapp/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.vanessali.bookingapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final int CREATE_ACC = 2;
@@ -34,6 +36,13 @@ public class MainActivity extends AppCompatActivity {
         createAcc = findViewById(R.id.text_create_account);
         forgotPass = findViewById(R.id.text_forgot_pass);
 
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputValidation();
+            }
+        });
+
 
         createAcc.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +61,50 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    private boolean emailValidation(){
+
+        String emailInput =emailEdt.getEditableText().toString().trim();
+
+        if(emailInput.isEmpty()){
+            emailEdt.setError("Field cannot be empty");
+            return false;
+        } else if(!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()){
+            // we return here true or false to indicate whether the user has inserted a valid email
+            // so if it does not match print error message
+            emailEdt.setError("Please enter a valid email address");
+            return false;
+        } else {
+            emailEdt.setError(null);
+            return true;
+        }
+
+
+    }
+
+    private boolean passwordValidation(){
+        String passInput = passwordEdt.getEditableText().toString().trim();
+
+        if(passInput.isEmpty()){
+            passwordEdt.setError("Field cannot be empty");
+            return false;
+        }else {
+            passwordEdt.setError(null);
+            return true;
+        }
+
+
+    }
+
+    public void inputValidation(){
+        if(!emailValidation()| !passwordValidation()){
+            Toast.makeText(this,"error checking", Toast.LENGTH_LONG).show();
+        }else{ // if user inputs everything correctly go to next activity
+            launchNextActivity();
+        }
+    }
+
+
 
 
 
